69ef8f404e60482718e447730d0cf0e694f76b26
Ramesh Balasekaran <rameshb@umd.edu>
2016-05-10 17:34:30 -0400

    LIBWEB-3193. Camel Integration with Solr.
    Added option to load the file and file type using Tika
    
    https://issues.umd.edu/browse/LIBWEB-3193?filter=-1

--- src/main/java/edu/umd/lib/process/SolrFileProcessor.java
+++ src/main/java/edu/umd/lib/process/SolrFileProcessor.java
@@ -1,24 +1,51 @@
 package edu.umd.lib.process;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.json.JSONObject;
+import org.xml.sax.SAXException;
 
 public class SolrFileProcessor implements Processor{
+	
 	Random rand = new Random();
+	Tika tika = new Tika();
+	
 	public void process(Exchange exchange) throws Exception {
 		
 		int randomID = rand.nextInt(200) + 1;
 		File file = exchange.getIn().getBody(File.class);
         JSONObject json = new JSONObject();
         json.put("id", randomID);
-        json.put("name", file.getName());        
-        json.put("type", "TextDocument");
+        json.put("name", file.getName());   
+        json.put("type", tika.detect(file));
+        json.put("fileContent", parseToPlainText(file));
         exchange.getIn().setBody("["+json.toString()+"]");        
 	}
         
+	public String parseToPlainText(File file) throws IOException, SAXException, TikaException {
+	    BodyContentHandler handler = new BodyContentHandler();
+	 
+	    AutoDetectParser parser = new AutoDetectParser();
+	    Metadata metadata = new Metadata();
+	    try{
+	    	InputStream targetStream = new FileInputStream(file.getAbsolutePath());
+	    	parser.parse(targetStream, handler, metadata);
+	    	return handler.toString();
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    	 return "Empty String";
+	    }
+	}
 	
 }
