a1d720a1cd15987e79abcc4442b0f849b9b0e786
Jeremy Matthew Gottwig <jgottwig@umd.edu>
2017-08-01 12:49:03 -0400

    LIBWEB-3846. Fixing Karaf issues.
    - Also, modifications to content parser.
    
    https://issues.umd.edu/browse/LIBWEB-3846

--- src/main/java/edu/umd/lib/process/DownloadProcessor.java
+++ src/main/java/edu/umd/lib/process/DownloadProcessor.java
@@ -12,10 +12,41 @@ import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+
+/**
+ * import org.apache.tika.parser.CompositeParser;
+ * import org.apache.tika.parser.ParseContext;
+ * import org.apache.tika.parser.pdf.*;
+ * import org.apache.tika.parser.txt.*;
+ * import org.apache.tika.parser.microsoft.*;
+ * import org.apache.tika.parser.html.*;
+ * import org.apache.tika.parser.rtf.*;
+ * import org.apache.tika.parser.xml.*;
+ * import org.apache.tika.parser.microsoft.ooxml.*;
+ * import org.apache.tika.parser.jpeg.*;
+ * import org.apache.tika.parser.image.*;
+ */
+
+import org.apache.tika.parser.CompositeParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.*;
+import org.apache.tika.parser.txt.*;
+import org.apache.tika.parser.microsoft.*;
+import org.apache.tika.parser.html.*;
+import org.apache.tika.parser.rtf.*;
+import org.apache.tika.parser.xml.*;
+import org.apache.tika.parser.microsoft.ooxml.*;
+import org.apache.tika.parser.jpeg.*;
+import org.apache.tika.parser.image.*;
+
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.parser.txt.TXTParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.json.JSONObject;
 import org.xml.sax.SAXException;
 
+@SuppressWarnings("unused")
 public class DownloadProcessor implements Processor {
 
   private static Logger log = Logger.getLogger(DownloadProcessor.class);
@@ -51,8 +82,9 @@ public class DownloadProcessor implements Processor {
     if (sourceType == "file" && "download".equals(action)) {
       Tika tika = new Tika();
       File destItem = new File(storagePath);
-      json.put("type", tika.detect(destItem));
-      json.put("fileContent", parseToPlainText(destItem));
+      String fileType = tika.detect(destItem);
+      json.put("type", fileType);
+      json.put("fileContent", parseToPlainText2(destItem, fileType));
       json.put("category", category);
     } else if ("file".equals(sourceType) && "delete".equals(action)) {
       json.put("fileStatus", fileStatus);
@@ -83,6 +115,47 @@ public class DownloadProcessor implements Processor {
       return handler.toString();
     } catch (Exception e) {
       e.printStackTrace();
+      return "Empty String";
+    }
+  }
+
+  /**
+   * In karaf these files are not auto loaded and should be included in
+   * BodyContentHandler handler = new BodyContentHandler(); imports even though
+   * not used explicitly import org.apache.tika.parser.AutoDetectParser; import
+   * org.apache.tika.parser.CompositeParser; import
+   * org.apache.tika.parser.ParseContext; import org.apache.tika.parser.pdf.*;
+   * import org.apache.tika.parser.txt.*; import
+   * org.apache.tika.parser.microsoft.*; import org.apache.tika.parser.html.*;
+   * import org.apache.tika.parser.rtf.*; import org.apache.tika.parser.xml.*;
+   * import org.apache.tika.parser.microsoft.ooxml.*; import
+   * org.apache.tika.parser.jpeg.*; import org.apache.tika.parser.image.*;
+   **/
+  public String parseToPlainText2(File file, String fileType) throws IOException, SAXException, TikaException {
+
+    AutoDetectParser parser = new AutoDetectParser();
+    BodyContentHandler handler = new BodyContentHandler();
+    Metadata metadata = new Metadata();
+    try {
+
+      ParseContext pcontext = new ParseContext();
+      InputStream targetStream = new FileInputStream(file.getAbsolutePath());
+
+      if (fileType.equalsIgnoreCase("text/plain")) {
+        TXTParser TexTParser = new TXTParser();
+        TexTParser.parse(targetStream, handler, metadata, pcontext);
+      } else if (fileType.equalsIgnoreCase("application/pdf")) {
+        PDFParser pdfparser = new PDFParser();
+        pdfparser.parse(targetStream, handler, metadata, pcontext);
+      } else {
+        parser.parse(targetStream, handler, metadata, pcontext);
+      }
+      if (handler.toString().equalsIgnoreCase("")) {
+        log.info("Error Extracting file content for fileType: " + fileType);
+      }
+      return handler.toString();
+    } catch (Exception e) {
+      log.info("Error:" + e.getMessage() + " for file of type :" + fileType);
       return "Empty String";
     }
   }
